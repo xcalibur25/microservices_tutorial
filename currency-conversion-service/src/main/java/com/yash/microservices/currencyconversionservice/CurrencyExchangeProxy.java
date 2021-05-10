8d8ef8aa82827f8fedf4c6916cb07e4ecca9b1af
Yash Raorane <yashraorane25@gmail.com>
2021-05-10 11:43:24 +0530

    commit load balancing

--- currency-conversion-service/src/main/java/com/yash/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/yash/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 
-@FeignClient(name = "currency-exchange",url = "localhost:8000")
+//@FeignClient(name = "currency-exchange",url = "localhost:8000")
+// we want feign client to talk to eureka and pick up the instance of Currency Exchange and do the load balancing.
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 	//we have created Currencyconversion bean matching the reponse of CurrencyExchange
 	@GetMapping("/currency-exchage/from/{from}/to/{to}")
