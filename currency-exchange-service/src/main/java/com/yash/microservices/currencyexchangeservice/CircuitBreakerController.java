ab69365f0ac0b3dfc80e8e16881e78b20533a9ce
Yash Raorane <yashraorane25@gmail.com>
2021-05-11 12:55:40 +0530

    commit circuit breaker

--- currency-exchange-service/src/main/java/com/yash/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/yash/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,7 +17,11 @@ public class CircuitBreakerController {
 
 	private Logger logger=LoggerFactory.getLogger(CircuitBreakerController.class);
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api",fallbackMethod = "hardcodedResponse") //retry 3 times.
+	//@Retry(name = "sample-api",fallbackMethod = "hardcodedResponse") //retry 3 times.
+	@CircuitBreaker(name = "default",fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name = "default")
+	@Bulkhead(name = "default")
+	//10s->10000 calls to api
 	public String sampleApi() {
 		logger.info("Sample Api Called Received");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/somedummy-url", String.class);
